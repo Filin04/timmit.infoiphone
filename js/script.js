89b71b86fd35f24d42193a21c64b63a4f73b4759
F06 <77489695+Filin04@users.noreply.github.com>
2024-05-31 06:16:04 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+$(document).ready(function () {
+    $(".contentBlock").hover(
+        function () {
+            $(this).children('.flex').children('.text').toggleClass('active');
+            $(this).children('.flex').children('.text').slideToggle(400);
 
+        },
+        function () {
+            $(this).children('.flex').children('.text').slideToggle(400);
+        });
+
+});
